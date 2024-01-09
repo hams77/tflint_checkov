4656123325f327e14de5a54734b76634603bb5e7
hams77 <99508424+hams77@users.noreply.github.com>
2024-01-09 23:24:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
+  ebs_optimized = true
   ami                    = var.ami
   instance_type          = var.instance_type
   availability_zone      = var.availability_zone
