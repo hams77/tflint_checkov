90d7f8ef5af623ea496509b944afcf336628d4fc
hams77 <99508424+hams77@users.noreply.github.com>
2024-01-15 00:27:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,4 +12,15 @@ resource "aws_instance" "example" {
   security_groups        = var.security_groups
   subnet_id              = var.subnet_id
   tags                   = var.tags
+  monitoring           = var.monitoring
+  dynamic "metadata_options" {
+    for_each = length(var.metadata_options) > 0 ? [var.metadata_options] : []
+
+    content {
+      http_endpoint               = try(metadata_options.value.http_endpoint, "enabled")
+      http_tokens                 = try(metadata_options.value.http_tokens, "optional")
+      http_put_response_hop_limit = try(metadata_options.value.http_put_response_hop_limit, 1)
+      instance_metadata_tags      = try(metadata_options.value.instance_metadata_tags, null)
+    }
+  }
 }
